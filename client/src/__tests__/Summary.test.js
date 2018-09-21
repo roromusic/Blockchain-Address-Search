50c52de55ab0981d21dd96d6ba9833d0510182c0
roromusic <roromusic@ucla.edu>
2018-09-20 20:05:28 -0700

    add Summary test

---
+++ client/src/__tests__/Summary.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, cleanup, getAllByTestId } from "react-testing-library";
+import Summary from "../components/Summary";
+
+afterEach(cleanup);
+
+test("Summary component renders user, n_tx, and final_balance props", () => {
+  const fakeUser = "lol";
+  const fake_n_tx = "10000";
+  const fake_final_balance = "1337";
+  const { getByText } = render(
+    <Summary
+      user={fakeUser}
+      n_tx={fake_n_tx}
+      final_balance={fake_final_balance}
+    />
+  );
+  const address = getByText(/address/i);
+  const transactions = getByText(/transactions/i);
+  const balance = getByText(/final balance/i);
+
+  expect(address.nextSibling.innerHTML).toBe(fakeUser);
+  expect(transactions.nextSibling.innerHTML).toEqual(fake_n_tx);
+  expect(balance.nextSibling.innerHTML).toEqual(fake_final_balance);
+});
